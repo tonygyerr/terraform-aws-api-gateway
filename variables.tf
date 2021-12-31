5b6f6415895cea758abd7b017df1e728ab6ed970
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-12-31 05:19:45 -0500

    variables.tf

--- variables.tf
+++ variables.tf
@@ -120,6 +120,12 @@ variable "environment" {
   default = ""
 }
 
+variable "environment_variables" {
+  description = "A map that defines environment variables for the Lambda Function."
+  type        = map(string)
+  default     = {}
+}
+
 variable "module_name" {
   type    = string
   default = ""
