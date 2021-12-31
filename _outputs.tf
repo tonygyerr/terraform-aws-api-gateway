b042adb8dc8ef1eba2b9dc7844aa55b32eaa2ed7
Gyepi-Garbrah, Ekow <ekow.gyepi-garbrah@accenture.com>
2021-12-31 13:47:05 -0500

    apigw

--- _outputs.tf
+++ _outputs.tf
@@ -17,7 +17,3 @@ output "lambda_invoke_arn" {
 output "lambda_source_arn" {
   value = aws_lambda_permission.this.source_arn
 }
-
-# output "lambda_swagger_full_arn" {
-#   value = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.swagger.arn}/invocations"
-# }
